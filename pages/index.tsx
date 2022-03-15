bcc33374a594e6baf3abbff2946f050017f4acee
Alam <alam.15@outlook.com>
2022-03-15 11:22:52 -0400

    add seed data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,31 @@
-import type { NextPage } from "next";
+import type { NextPage, GetStaticProps } from "next";
+import Head from "next/head";
+
 import styles from "../styles/Home.module.css";
 
-const Home: NextPage = () => {
-  return <h1>Linktree</h1>;
+import Account from "../shared/models/Account";
+
+const Home: NextPage<Account> = props => {
+  return (
+    <>
+      <Head>
+        <title>Linktree</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div></div>
+    </>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const fs = require("fs");
+
+  const rawData = await fs.readFileSync("data/seed.json");
+  const data = JSON.parse(rawData);
+
+  return {
+    props: data["account"],
+  };
 };
 
 export default Home;
