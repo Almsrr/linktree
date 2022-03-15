c15a52b498972aab87695a5e4235c7c72bbd4483
Alam <alam.15@outlook.com>
2022-03-15 11:36:40 -0400

    add header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,30 @@
 import type { NextPage, GetStaticProps } from "next";
 import Head from "next/head";
+import Image from "next/image";
 
 import styles from "../styles/Home.module.css";
+import profilePhoto from "../assets/images/palas-pingpong.jpeg";
 
 import Account from "../shared/models/Account";
 
-const Home: NextPage<Account> = props => {
+const Home: NextPage<Account> = ({ name, bio, links }) => {
   return (
     <>
       <Head>
         <title>Linktree</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div></div>
+      <div className={styles.page}>
+        <div className={styles.container}>
+          <header className={styles.header}>
+            <div className={styles.img}>
+              <Image src={profilePhoto} alt="profile-photo" />
+            </div>
+            <h1 className={styles.name}>{name}</h1>
+            <p className={styles.bio}>{bio}</p>
+          </header>
+        </div>
+      </div>
     </>
   );
 };
